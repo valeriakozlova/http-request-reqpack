5a0978b4d7d54743254de65bf4b1b7d67f508ac3
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-10-29 21:01:32 +0000

    Committing with the JS file

--- practice.js
+++
@@ -1,7 +0,0 @@
-
-var request = require('request');
-
-request('https://sytantris.github.io/http-examples/', function(err, response, body) {
-  if (err) throw err;
-  console.log('Response Status Code:', response.statusCode);
-});
