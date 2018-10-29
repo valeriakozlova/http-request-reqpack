748b78d47431703bee17ba6fa9e329e412ee6c8e
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-10-29 21:17:18 +0000

    Committing last changes

--- downloadafile.js
+++ downloadafile.js
@@ -7,5 +7,9 @@ request.get('https://sytantris.github.io/http-examples/future.jpg')
        })
        .on('response', function (response) {
          console.log('Response: ', response.statusCode, response.statusMessage, response.headers['content-type']);
+         console.log('Downloading image...');
        })
-       .pipe(fs.createWriteStream('./future.jpg'));
+       .pipe(fs.createWriteStream('./future.jpg'))
+       .on('finish', function () {
+        console.log('Download complete.');
+       });
